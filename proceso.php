95e1fb6bc402c3f3a8b47eb648c4567f5d5000b8
Melanyar12 <melanyarmijos2003@gmail.com>
2024-09-15 02:38:09 -0500

    Add files via upload

---
+++ proceso.php
@@ -0,0 +1,15 @@
+<?php 
+ $nombre = $_REQUEST['nombre'];
+ $edad = $_REQUEST['edad'];
+ $contraseña = $_REQUEST['contraseña'];
+ $fecha = $_REQUEST['fecha'];
+ $correo = $_REQUEST['correo'];
+
+ echo "Nombre: $nombre"; <br>
+ echo "Edad: $edad"; <br>
+ echo "Contraseña: $contraseña";<br>
+ echo "Fecha: $fecha"; <br>
+ echo "Correo: $correo"; <br>
+
+
+?>
